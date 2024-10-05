3a32ab983d1f481a89fd6c1bf6dcc8f05c1316e9
yu <yul1024@outlook.com>
2024-10-05 19:44:56 +0800

    Update checkpoint_tool.py

--- utils/checkpoint_tool.py
+++ utils/checkpoint_tool.py
@@ -3,23 +3,25 @@ import torch
 from pathlib import Path
 
 
-def save_checkpoint(path_to_save, model, optimizer, ):
+def save_checkpoint(path_to_save, model, optimizer, scheduler=None):
     file_path = Path(path_to_save)
     file_path.parent.mkdir(parents=True, exist_ok=True)
     torch.save({
         'model': model.state_dict(),
         'optimizer': optimizer.state_dict(),
-        # 'scheduler': scheduler.state_dict(),
+        'scheduler': scheduler.state_dict(),
     }, file_path)
 
 
-def load_checkpoint(path_to_checkpoint, model, optimizer, ):
+def load_checkpoint(path_to_checkpoint, model, optimizer, scheduler=None):
     checkpoint = torch.load(path_to_checkpoint)
     model.load_state_dict(checkpoint['model'])
     optimizer.load_state_dict(checkpoint['optimizer'])
-    # if scheduler is not None:
-    #     scheduler.load_state_dict(checkpoint['scheduler'])
-    return  model, optimizer,  # scheduler
+    if scheduler is not None:
+        scheduler.load_state_dict(checkpoint['scheduler'])
+        return model, optimizer, scheduler
+    else:
+        return  model, optimizer
 
 
 if __name__ == '__main__':
