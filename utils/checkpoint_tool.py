5bf982a6d64740fa0d8347894307275a3fb40c9b
yu <yul1024@outlook.com>
2024-10-05 21:08:13 +0800

    Update checkpoint_tool.py

--- utils/checkpoint_tool.py
+++ utils/checkpoint_tool.py
@@ -2,6 +2,13 @@ import torch
 
 from pathlib import Path
 
+"""
+这里是强化的对于checkpoint的保存和加载。
+默认保存model、optimizer、scheduler。
+
+设计了没有scheduler的情况，但是按照我的trainer的方法，scheduler是一定有的。
+"""
+
 
 def save_checkpoint(path_to_save, model, optimizer, scheduler=None):
     file_path = Path(path_to_save)
@@ -21,7 +28,7 @@ def load_checkpoint(path_to_checkpoint, model, optimizer, scheduler=None):
         scheduler.load_state_dict(checkpoint['scheduler'])
         return model, optimizer, scheduler
     else:
-        return  model, optimizer
+        return model, optimizer
 
 
 if __name__ == '__main__':
