8114070da65ae461079214f88a8cb97284757da1
yu <yul1024@outlook.com>
2024-11-26 13:59:36 +0800

    Update dataset.py

--- dataloader/dataset/dataset.py
+++ dataloader/dataset/dataset.py
@@ -1,2 +1,18 @@
+from torch.utils.data import Dataset
 
+
+class MyDataset(Dataset):
+    def __init__(self, config: dict):
+        pass
+
+    def __len__(self):
+        pass
+        # return len()
+
+    def __getitem__(self, index):
+        pass
+
+
+if __name__ == '__main__':
+    pass
 
