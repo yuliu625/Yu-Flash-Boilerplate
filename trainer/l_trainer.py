086eedb1dbe83821220d96532cb08b7d16cf7f36
yu <yul1024@outlook.com>
2024-11-27 10:20:26 +0800

    Update l_trainer.py

--- trainer/l_trainer.py
+++ trainer/l_trainer.py
@@ -5,12 +5,13 @@ import torch
 import lightning as pl
 
 from pathlib import Path
-from omegaconf import OmegaConf
+from omegaconf import OmegaConf, DictConfig
 import hydra
 
 
 class LightningTrainer:
-    def __init__(self, config):
+    @hydra.main(config_path=str(Path(__file__).parent / "configs"), config_name="config")
+    def __init__(self, config: DictConfig) -> None:
         self.config = config
         self.trainer = pl.Trainer()
 
