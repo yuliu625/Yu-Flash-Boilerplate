fcba7586d9bebee48e4f5c46f896b802d39e2516
yu <yul1024@outlook.com>
2024-11-27 10:19:18 +0800

    finished

--- utils/hf_download.sh
+++ utils/hf_download.sh
@@ -1,4 +1,6 @@
+#旧的我使用命令行在huggingface上下载模型和数据集的脚本。
 conda activate dl_env
-huggingface-cli download --resume-download ${} --local-dir /home/liuyu/liuyu_data/models/hf/${}/${}
-
-huggingface-cli download --repo-type dataset --resume-download wikitext --local-dir wikitext
+#这个是下载模型的。
+huggingface-cli download --resume-download ${} --local-dir /home/models/hf/${}/${}
+#这个是下载数据集的。
+huggingface-cli download --repo-type dataset --resume-download ${} --local-dir ${}
