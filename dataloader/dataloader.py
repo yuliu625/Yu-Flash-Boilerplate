37c97910022da2ccc3f04e591a99df41fc306808
yu <yul1024@outlook.com>
2024-11-28 20:05:36 +0800

    Update dataloader.py

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -4,8 +4,21 @@ from dataloader.collate_fn import collate_fn
 from torch.utils.data import DataLoader
 
 
-def build_dataloader(dataset,):
-    pass
+class DemoDataLoader:
+    def __init__(self, dataset, batch_size=1, shuffle=True, num_workers=0, collate_fn=collate_fn):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+
+    def get_train_dataloader(self):
+        pass
+
+    def get_val_dataloader(self):
+        pass
+
+
+# def build_dataloader(dataset,):
+#     pass
 
 
 if __name__ == '__main__':
