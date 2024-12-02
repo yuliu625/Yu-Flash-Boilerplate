af862464c03f1b6dd383221bd0bf34a9a3e60696
yu <yul1024@outlook.com>
2024-12-02 19:40:37 +0800

    Create processor.py

---
+++ dataloader/dataset/processor.py
@@ -0,0 +1,18 @@
+"""
+所有模型都会有处理流。
+
+将原本的数据转换为tensor。
+"""
+
+import torch
+
+from pathlib import Path
+
+
+class Processor:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
