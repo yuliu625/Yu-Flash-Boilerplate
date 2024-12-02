079a8f0d32132ed0a8e5d517590cfb23663a8f1e
yu <yul1024@outlook.com>
2024-12-02 19:40:50 +0800

    Update l_model.py

--- trainer/l_model.py
+++ trainer/l_model.py
@@ -1,16 +1,33 @@
+"""
+基于lightning的模型定义。
+
+在我的设计中，model的定义依然是直接使用nn.Module来实现的，模型的设置也在其中实现。
+仅在模型完整的实现之后，才在LightningModule中组合。
+目的在于，低耦合，便于迁移。
+
+这里仅设置干净的step相关内容。
+"""
+
 # from model import
 
 import torch
+import torch.nn as nn
 import torch.optim as optim
+
 import lightning as pl
+import torchmetrics
 
 
 class LightningModel(pl.LightningModule):
-    def __init__(self, model, loss_fn, config):
+    def __init__(self, config):
         super().__init__()
         self.config = config
-        self.model = model
-        self.loss_fn = loss_fn
+        self.model_config = config['model']
+
+        # 设置模型
+        self.model = MyModel(self.model_config)
+        # 设置损失函数
+        self.loss_fn = self.choose_loss_fn(config['loss_fn'])
 
         # 额外的设置
         # 保存模型配置的超参数。
@@ -19,12 +36,15 @@ class LightningModel(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         """必要的，实现每一步的训练。但是batch_idx似乎用不到，是lightning会用的。"""
 
-        # 前向传播
-        x, y = batch
-        y_hat = self.model(x)
+        # 获取数据
+        labels = batch['labels']
+        inputs = batch['datas']
 
-        # 计算损失
-        loss = self.loss_fn(y_hat, y)
+        # 前向传播
+        outputs = self.model(inputs)
+
+        # 测试损失
+        loss = self.loss_fn(outputs, labels)
 
         # 日志
         self.log('train_loss', loss)
@@ -38,17 +58,24 @@ class LightningModel(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         """应该的，验证模型。"""
 
+        # 获取数据
+        labels = batch['labels']
+        inputs = batch['datas']
+
         # 前向传播
-        x, y = batch
-        y_hat = self.model(x)
+        outputs = self.model(inputs)
 
         # 测试损失
-        loss = self.loss_fn(y_hat, y)
+        loss = self.loss_fn(outputs, labels)
 
         # 这里应该还有测试指标。
 
         # 日志
         self.log('val_loss', loss)
+
+    def choose_loss_fn(self, choice: str):
+        if choice == 'cross_entropy':
+            return nn.CrossEntropyLoss()
 
 
 if __name__ == '__main__':
