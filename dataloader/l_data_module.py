7ca061eea3979b141e9a2cd77c119409c59c348e
yu <yul1024@outlook.com>
2024-12-02 19:40:41 +0800

    Update l_data_module.py

--- dataloader/l_data_module.py
+++ dataloader/l_data_module.py
@@ -1,3 +1,37 @@
+"""
+lightning中的DataModule。
+
+这个类可以代替torch中原本的dataloader相关的设置，在分布式环境下有很大好处。
+但是为了通用性，我依然更多的使用原本的dataloader来定义数据的加载和处理。
+"""
+
+import torch
+
+import lightning as pl
+from torch.utils.data import DataLoader
+
+
+class LDataModule(pl.LightningDataModule):
+    def __init__(self, hparams):
+        super().__init__()
+
+    def setup(self, stage=None):
+        pass
+
+    def train_dataloader(self):
+        return DataLoader()
+
+    def val_dataloader(self):
+        return DataLoader()
+
+    def test_dataloader(self):
+        return DataLoader()
+
+    def predict_dataloader(self):
+        return DataLoader()
+
+    def teardown(self, stage: str) -> None:
+        pass
 
 
 if __name__ == '__main__':
