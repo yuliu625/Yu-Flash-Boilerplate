49296775ae89e08210dd333f2b0ee3654c910e14
yu <yul1024@outlook.com>
2024-12-05 18:49:10 +0800

    Update run_dataloader.py

--- tsts/run_dataloader.py
+++ tsts/run_dataloader.py
@@ -5,7 +5,12 @@
     - collate_fn的工作情况。确保batch的正确。
 """
 
+from dataloader import DataLoaderFactory
 from torch.utils.data import DataLoader
+
+
+def main():
+    pass
 
 
 if __name__ == '__main__':
