b242bd4a2dfdbb23b199937727b2388c29a40b43
yu <yul1024@outlook.com>
2024-12-05 18:49:13 +0800

    Update run_config.py

--- tsts/run_config.py
+++ tsts/run_config.py
@@ -7,9 +7,12 @@ from omegaconf import OmegaConf
 
 
 @hydra.main(config_path='conf', config_name='test_config')
-def test_config(config):
+def print_config(config):
+    # 打印解析后的配置文件，可以检查配置文件的原本的情况。
     print(OmegaConf.to_yaml(config, resolve=True))
+    # 打印对象格式的配置文件，方便在每一步测试。
+    print(OmegaConf.to_object(config))
 
 
 if __name__ == '__main__':
-    test_config()
+    print_config()
