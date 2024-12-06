3bfe2efacd255e71c9669d2d1201a1e8f628b7e3
yu <yul1024@outlook.com>
2024-12-06 16:16:02 +0800

    Update processor.py

--- dataloader/dataset/processor.py
+++ dataloader/dataset/processor.py
@@ -1,8 +1,11 @@
 """
-所有模型都会有处理流。
+所有模型都会有处理流，将原本的数据转换为tensor。
 
-将原本的数据转换为tensor。
+这里可能会涉及很多工具，这些都是在cpu上运行的。
+如果使用transformers中的工具，需要进行导入。
 """
+
+from transformers import AutoProcessor
 
 import torch
 
@@ -10,8 +13,16 @@ from pathlib import Path
 
 
 class Processor:
-    def __init__(self):
-        pass
+    """
+    original data的处理方法。
+
+    这里以工具的方式将processor与原本的dataset类分离。
+    """
+    def __init__(self, config: dict):
+        self.config = config
+
+    def __call__(self, data) -> torch.Tensor:
+        """主要的方法，输入原本的数据，输出可供模型计算的tensor。"""
 
 
 if __name__ == '__main__':
