318658679174e62cba13000066cb000af80add3d
yu <yul1024@outlook.com>
2024-12-09 10:24:12 +0800

    Update collate_fn.py

--- dataloader/collate_fn.py
+++ dataloader/collate_fn.py
@@ -28,4 +28,19 @@ def collate_fn(batch):
 
 
 if __name__ == '__main__':
-    pass
+    """在这里测试dataloader及其collate_fn的情况。"""
+    from dataset import CommonDataset
+    from torch.utils.data import DataLoader
+
+    # 这里可以直接使用dataset包中已经测试通过的dataset和其config。
+    common_dataset = CommonDataset({
+        'control_file': {'path': r''},
+        'file': {'file_dir_path': r''},
+        'processor': {'path': r''},
+    })
+
+    # 只要输出一次，batch_size大于1，并且有填充。
+    dataloader = DataLoader(common_dataset, batch_size=2, collate_fn=collate_fn)
+    for batch in dataloader:
+        print(batch)
+        break
