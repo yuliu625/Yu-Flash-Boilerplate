a8dec5423a331c2604f2c78c8ef8063df3d0fc9e
yu <yul1024@outlook.com>
2024-12-09 10:24:30 +0800

    Update run_dataloader.py

--- tsts/run_dataloader.py
+++ tsts/run_dataloader.py
@@ -5,13 +5,22 @@
     - collate_fn的工作情况。确保batch的正确。
 """
 
-from dataloader import DataLoaderFactory
+from .run_dataset import get_dataset
+
+from dataloader.collate_fn import collate_fn
+
 from torch.utils.data import DataLoader
 
 
-def main():
-    pass
+def check_dataloader(config):
+    common_dataset = get_dataset(config)
+
+    dataloader = DataLoader(common_dataset, batch_size=2, collate_fn=collate_fn)
+    for batch in dataloader:
+        print(batch)
+        break
 
 
 if __name__ == '__main__':
-    pass
+    """这里指定config。之后会改进为从命令行或配置文件自动读取。"""
+    check_dataloader({})
