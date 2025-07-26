56140d276808362cbef4d8a0c3cbdc95bb13ba3e
yu <yul1024@outlook.com>
2025-07-26 21:42:33 +0800

    Update collate_fn.py

--- dataloader/collate_fn.py
+++ dataloader/collate_fn.py
@@ -1,14 +1,22 @@
 """
-dataloader的collate_fn很重要。
+dataloader会使用的collate_fn。
 
-经过collate_fn：
-    - 原本键的名称会改变，我的方式是设置为复数。
-    - batch中不同的length会被padding。
-我的做法，会以dict返回。
+collate_fn的功能应该具有:
+    - 以list进入的独立的值转换为一组。
+    - 不同长度的序列使用相关padding方法处理为相同长度。
+
+约定:
+    - 区别名称: 进入数据以单数命名，collate_fn处理后的数据以复数命名。
+    - 可使用dict。复杂情况，使用dict进行控制。
 """
+
+from __future__ import annotations
 
 import torch
 from torch.nn.utils.rnn import pad_sequence
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
 
 
 def collate_fn(batch):
