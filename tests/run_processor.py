f65cf181f0ebae634d849aefbde4426769a83c57
yu <yul1024@outlook.com>
2025-07-27 20:39:53 +0800

    Create run_processor.py

---
+++ tests/run_processor.py
@@ -0,0 +1,20 @@
+"""
+实例化一个processor，查看其对于数据的转换。
+"""
+
+from deep_learning_project.torch_dataloaders.torch_datasets.processor import Processor
+
+from pathlib import Path
+
+
+def check_processor(config, data_path_str: str):
+    processor = Processor(config)
+    data_path = Path(data_path_str)
+    result = processor(data_path)
+    return result
+
+
+if __name__ == '__main__':
+    """这里需要指定processor的设置，并指定测试数据的路径。"""
+    result = check_processor({}, r'')
+    print(result)
