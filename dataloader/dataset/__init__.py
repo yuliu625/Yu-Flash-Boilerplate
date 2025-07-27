08fda21b94b063f0fb28692e84edc326b19158c5
yu <yul1024@outlook.com>
2025-07-27 20:39:02 +0800

    rename

--- dataloader/dataset/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-数据集的定义，封装了数据的加载。会在dataloader包中直接使用。
-"""
-
-# __all__ = []
-
-
-# 一般情况下，仅对外暴露定义的相关dataset，供dataloader使用。
-from .datasets import CommonDataset
--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-积累的常见任务中都可以复用的工具。
-"""
-
-# __all__ = []
-
-
-from .checkpoint_tool import save_checkpoint, load_checkpoint
-
