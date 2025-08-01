ffb397f2baa2bf4ec9f759cbd55f9bf391800c79
yu <yul1024@outlook.com>
2025-08-01 16:30:16 +0800

    Update torch_dataset_factory.py

--- deep_learning_project/torch_dataloaders/torch_datasets/torch_dataset_factory.py
+++ deep_learning_project/torch_dataloaders/torch_datasets/torch_dataset_factory.py
@@ -1,8 +1,13 @@
 """
 构建dataset的工厂。
+
+以完全序列化的方法获取dataset对象，从而极大简化后续所有工程和测试。
 """
 
 from __future__ import annotations
+
+# 同一文件夹下导入定义的各种dataset。
+# from .torch_datasets import
 
 from torch.utils.data import Dataset
 
@@ -11,6 +16,18 @@ from typing import TYPE_CHECKING
 
 
 class DatasetFactory:
+    @staticmethod
+    def create_train_dataset(
+
+    ) -> Dataset:
+        ...
+
+    @staticmethod
+    def create_val_dataset(
+
+    ) -> Dataset:
+        ...
+
     @staticmethod
     def create_dataset(
 
