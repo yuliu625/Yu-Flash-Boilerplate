3ba05a25ffd4f5ad21b82e9070c4b17c607a6e09
yu <yul1024@outlook.com>
2025-08-01 16:23:51 +0800

    Create test_models.py

---
+++ tests/unit_tests/test_models/test_models.py
@@ -0,0 +1,22 @@
+"""
+测试整体模型的输入输出形状是否复合预期。
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+# """在这里模拟输入，不需要真的使用dataloader。"""
+# torch.Size([1, 19, 3, 224, 224])
+# example_input_array = torch.rand(1, 5, 3, 224, 224)
+# model = NormalModel(
+#     {
+#
+#     }
+# )
+# outputs = model(example_input_array)
+# print(outputs)
+# print(outputs.shape)
+
