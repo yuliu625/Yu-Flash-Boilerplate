454d8703685da5a0a774db0c15c3d22b8ad18b4f
yu <yul1024@outlook.com>
2025-08-06 17:48:58 +0800

    Update l_callback.py

--- deep_learning_project/trainer/l_callback.py
+++ deep_learning_project/trainer/l_callback.py
@@ -9,12 +9,14 @@ from __future__ import annotations
 
 from lightning.pytorch.callbacks import (
     ModelSummary,
+    DeviceStatsMonitor,
     ModelCheckpoint,
     EarlyStopping,
 )
 
 from typing import TYPE_CHECKING
-# if TYPE_CHECKING:
+if TYPE_CHECKING:
+    from lightning.pytorch.callbacks import Callback
 
 
 class CallbackFactory:
@@ -67,4 +69,42 @@ class CallbackFactory:
             patience=5,
         )
         return early_stop_callback
+
+
+class LCallbackFactory:
+    @staticmethod
+    def create_model_checkpoint_callback(
+        callback_kwargs: dict,
+    ) -> Callback:
+        model_checkpoint_callback = ModelCheckpoint(
+            **callback_kwargs,
+        )
+        return model_checkpoint_callback
+
+    @staticmethod
+    def create_early_stopping_callback(
+        callback_kwargs: dict,
+    ) -> Callback:
+        early_stopping_callback = EarlyStopping(
+            **callback_kwargs,
+        )
+        return early_stopping_callback
+
+    @staticmethod
+    def create_model_summary_callback(
+        callback_kwargs: dict,
+    ) -> Callback:
+        model_summary_callback = ModelSummary(
+            **callback_kwargs,
+        )
+        return model_summary_callback
+
+    @staticmethod
+    def create_device_stats_monitor_callback(
+        callback_kwargs: dict,
+    ) -> Callback:
+        device_stats_monitor_callback = DeviceStatsMonitor(
+            **callback_kwargs,
+        )
+        return device_stats_monitor_callback
 
