2411c3b006dcbb2fdc329b98c17b0b1e0e97d72f
yu <yul1024@outlook.com>
2025-08-07 12:18:09 +0800

    Update train.py

--- deep_learning_project/train.py
+++ deep_learning_project/train.py
@@ -6,3 +6,24 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
+
+
+def get_data_module():
+    ...
+
+
+def get_model():
+    ...
+
+
+def get_trainer():
+    ...
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    pass
+
