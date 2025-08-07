8278c7ea7816e2b1e53a7f5a16f45e6f7e9417c3
yu <yul1024@outlook.com>
2025-08-07 20:10:19 +0800

    Update train.py

--- deep_learning_project/train.py
+++ deep_learning_project/train.py
@@ -5,10 +5,12 @@
 from __future__ import annotations
 
 from .trainer.l_data_module import LDataModule
+from .trainer.l_model_factory import LModelFactory
+from .trainer.l_trainer_builder import LTrainerBuilder
 
 from omegaconf import OmegaConf
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 if TYPE_CHECKING:
     from lightning import LightningDataModule, LightningModule, Trainer
     from omegaconf import DictConfig
@@ -28,17 +30,43 @@ def get_model(
 
 
 def get_trainer(
+    mode: Literal['train', 'fast_dev_run'],
     trainer_config: dict,
 ) -> Trainer:
-    ...
+    trainer = LTrainerBuilder.build_lightning_trainer(
+        mode=mode,
+        trainer_config=trainer_config,
+    )
+    return trainer
 
 
 def main(
+    mode: Literal['train', 'fast_dev_run', 'resume'],
     experiment_config: dict,
 ):
-    ...
+    data_module = get_data_module(
+        data_module_config=experiment_config['data_module_config'],
+    )
+    model = get_model(
+        model_config=experiment_config['model_config'],
+    )
+    trainer = get_trainer(
+        mode=mode,
+        trainer_config=experiment_config['trainer_config'],
+    )
+    # 进行训练
+    trainer.fit(
+        model=model,
+        datamodule=data_module,
+    )
 
 
 if __name__ == '__main__':
-    pass
+    mode_ = r'train'
+    experiment_config_ = {}
+
+    main(
+        mode=mode_,
+        experiment_config=experiment_config_,
+    )
 
