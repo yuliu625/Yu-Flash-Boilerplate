acce8f308115a9eb674ef02f2dca1f251e339a12
yu <yul1024@outlook.com>
2025-08-07 21:23:44 +0800

    Update loss_fn_factory.py

--- deep_learning_project/trainer/l_model_building_tools/loss_fn_factory.py
+++ deep_learning_project/trainer/l_model_building_tools/loss_fn_factory.py
@@ -1,28 +1,35 @@
 """
 基于原生torch，构建loss_fn。
+
+这个工厂方法构建的目的:
+    - 完全以可序列化数据构建metric。
 """
 
 from __future__ import annotations
 
 import torch
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 # if TYPE_CHECKING:
 
 
 class LossFnFactory:
+    # ====暴露方法。====
     @staticmethod
     def create_loss_fn(
-        loss_fn_name: str,
-        loss_fn_kwargs: dict,
+        loss_fn_name: Literal[
+            'cross_entropy', 'mse', 'binary_crossentropy'
+        ],
+        loss_fn_config: dict,
     ) -> torch.nn.Module:
-        ...
+        if loss_fn_name == 'cross_entropy':
+            return LossFnFactory.create_cross_entropy_loss_fn(loss_fn_config=loss_fn_config)
 
     @staticmethod
     def create_cross_entropy_loss_fn(
-        loss_fn_kwargs: dict,
+        loss_fn_config: dict,
     ) -> torch.nn.Module:
         return torch.nn.CrossEntropyLoss(
-            **loss_fn_kwargs,
+            **loss_fn_config,
         )
 
