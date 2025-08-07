1ace69a3db65e501129342bfe66a1bf42d21d1f2
yu <yul1024@outlook.com>
2025-08-07 17:01:55 +0800

    Update train.py

--- deep_learning_project/train.py
+++ deep_learning_project/train.py
@@ -4,23 +4,38 @@
 
 from __future__ import annotations
 
+from .trainer.l_data_module import LDataModule
+
+from omegaconf import OmegaConf
+
 from typing import TYPE_CHECKING
-# if TYPE_CHECKING:
+if TYPE_CHECKING:
+    from lightning import LightningDataModule, LightningModule, Trainer
+    from omegaconf import DictConfig
 
 
-def get_data_module():
+def get_data_module(
+    data_module_config: dict,
+) -> LightningDataModule:
+    data_module = LDataModule(**data_module_config)
+    return data_module
+
+
+def get_model(
+    model_config: dict,
+) -> LightningModule:
     ...
 
 
-def get_model():
+def get_trainer(
+    trainer_config: dict,
+) -> Trainer:
     ...
 
 
-def get_trainer():
-    ...
-
-
-def main():
+def main(
+    experiment_config: dict,
+):
     ...
 
 
