f260c13a8dbd42b30b652597c3e823561debb62d
yu <yul1024@outlook.com>
2025-08-07 17:02:24 +0800

    Update l_model_factory.py

--- deep_learning_project/trainer/l_model_factory.py
+++ deep_learning_project/trainer/l_model_factory.py
@@ -9,6 +9,17 @@
 
 from __future__ import annotations
 
+from .l_model_building_tools import (
+    LossFnFactory,
+    OptimizerClassFactory,
+    MetricFactory,
+)
+from .l_model import LModel
+
+from deep_learning_project.torch_models.torch_models import NormalModel
+
+import torch
+
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
 
@@ -16,7 +27,32 @@ from typing import TYPE_CHECKING
 class LModelFactory:
     @staticmethod
     def create_default_model(
-
+        loss_fn_name: str,
+        loss_fn_kwargs: dict,
+        optimizer_name: str,
+        optimizer_config: dict,
+        metrics_configs_list: list[dict]
     ):
-        ...
+        loss_fn = LossFnFactory.create_loss_fn(
+            loss_fn_name=loss_fn_name,
+            loss_fn_kwargs=loss_fn_kwargs,
+        )
+        optimizer_class = OptimizerClassFactory.create_optimizer_class(
+            optimizer_name=optimizer_name,
+        )
+        metrics_dict = {
+            metric_config['metric_name']: MetricFactory.create_metric(
+                metric_name=metric_config['metric_name'],
+                metric_kwargs=metric_config['metrics_kwargs'],
+            )
+            for metric_config in metrics_configs_list
+        }
+        l_model = LModel(
+            torch_model=NormalModel({}),
+            loss_fn=loss_fn,
+            optimizer_class=optimizer_class,
+            optimizer_configs=optimizer_config,
+            metrics_dict=metrics_dict,
+        )
+        return l_model
 
