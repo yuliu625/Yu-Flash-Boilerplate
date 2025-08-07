787ae186984248dcd7159e6f6264264d6ce4d0da
yu <yul1024@outlook.com>
2025-08-07 17:02:20 +0800

    Update loss_fn_factory.py

--- deep_learning_project/trainer/l_model_building_tools/loss_fn_factory.py
+++ deep_learning_project/trainer/l_model_building_tools/loss_fn_factory.py
@@ -1,15 +1,28 @@
 """
-构建损失函数的工厂。
+基于原生torch，构建loss_fn。
 """
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Callable
+import torch
+
+from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
 
 
 class LossFnFactory:
     @staticmethod
-    def create_fn():
+    def create_loss_fn(
+        loss_fn_name: str,
+        loss_fn_kwargs: dict,
+    ) -> torch.nn.Module:
         ...
+
+    @staticmethod
+    def create_cross_entropy_loss_fn(
+        loss_fn_kwargs: dict,
+    ) -> torch.nn.Module:
+        return torch.nn.CrossEntropyLoss(
+            **loss_fn_kwargs,
+        )
 
