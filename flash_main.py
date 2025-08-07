48120a8fea6a1a7b3c11e99704b2706dba2c6089
yu <yul1024@outlook.com>
2025-08-07 21:49:34 +0800

    Update flash_main.py

--- flash_main.py
+++ flash_main.py
@@ -2,18 +2,27 @@
 总入口。在这里启动模型的训练。
 """
 
-from trainer import LightningTrainer
+from __future__ import annotations
+
+from deep_learning_project.experiment_runner import ExperimentRunner
 
 from pathlib import Path
+from omegaconf import OmegaConf, DictConfig
 import hydra
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
 
 
 # 或许每次需要在这里修改配置文件的路径。
 @hydra.main(config_path=str(Path(__file__).parent / "configs"), config_name="config")
-def main(config):
-    l_trainer = LightningTrainer(config)
-    l_trainer.train()
+def main(experiment_config):
+    ExperimentRunner.run(
+        mode='train',
+        experiment_config=experiment_config,
+    )
 
 
 if __name__ == '__main__':
     main()
+
