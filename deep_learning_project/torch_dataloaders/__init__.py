151aeb0007f455529e12722a39fee1aae34cc94c
yu <yul1024@outlook.com>
2025-08-08 16:19:52 +0800

    Update __init__.py

--- deep_learning_project/torch_dataloaders/__init__.py
+++ deep_learning_project/torch_dataloaders/__init__.py
@@ -1,9 +1,6 @@
 """
-dataloader的定义，输出为batch的tensor。最终输入模型的数据格式。
+基于torch定义的数据加载器。
 """
 
-# __all__ = []
-
-
-# 暴露用工厂模式封装的dataloader获取类。或许用lightning的DataModule会更好？
 from .torch_dataloader_factory import DataLoaderFactory
+
