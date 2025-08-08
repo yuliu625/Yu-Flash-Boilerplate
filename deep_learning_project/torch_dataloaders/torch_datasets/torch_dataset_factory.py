4cffdd16d28645c77d62baf4e9c523a06c76008c
yu <yul1024@outlook.com>
2025-08-08 12:24:44 +0800

    Update torch_dataset_factory.py

--- deep_learning_project/torch_dataloaders/torch_datasets/torch_dataset_factory.py
+++ deep_learning_project/torch_dataloaders/torch_datasets/torch_dataset_factory.py
@@ -3,15 +3,20 @@
 
 以完全序列化的方法获取dataset对象，从而极大简化后续所有工程和测试。
 
-注意，这个方法直接和该项目中自定义的data-module对应，方法可以不真正实现，但是需要保留。
+注意:
+    - 这个方法直接和该项目中自定义的data-module对应，方法可以不真正实现，但是需要保留。
 """
 
 from __future__ import annotations
 
 # 同一文件夹下导入定义的各种dataset。
-# from .torch_datasets import
+from .torch_datasets import (
+    DFDataset,
+    ControlDataset,
+)
 
-from torch.utils.data import Dataset
+import torch
+from pathlib import Path
 
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
@@ -25,27 +30,27 @@ class DatasetFactory:
     @staticmethod
     def create_train_dataset(
         data_path,
-    ) -> Dataset:
+    ) -> torch.utils.data.Dataset:
         ...
 
     # ====暴露方法。需要实现的方法。====
     @staticmethod
     def create_validate_dataset(
         data_path,
-    ) -> Dataset:
+    ) -> torch.utils.data.Dataset:
         ...
 
     # ====暴露方法。需要实现的方法。====
     @staticmethod
     def create_test_dataset(
         data_path,
-    ) -> Dataset:
+    ) -> torch.utils.data.Dataset:
         ...
 
     # ====暴露方法。需要实现的方法。====
     @staticmethod
     def create_predict_dataset(
         data_path,
-    ) -> Dataset:
+    ) -> torch.utils.data.Dataset:
         ...
 
