2df9e36458812bdee1b8b907ca1276e7fc615461
yu <yul1024@outlook.com>
2025-08-08 16:12:54 +0800

    Update optimizer_class_factory.py

--- deep_learning_project/trainer/l_model_building_tools/optimizer_class_factory.py
+++ deep_learning_project/trainer/l_model_building_tools/optimizer_class_factory.py
@@ -6,16 +6,17 @@ from __future__ import annotations
 
 import torch
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 # if TYPE_CHECKING:
 
 
 class OptimizerClassFactory:
     @staticmethod
     def create_optimizer_class(
-        optimizer_name: str,
+        optimizer_name: Literal['Adam', 'SGD', 'RMSProp', 'RMSPropOptimizer'],
     ) -> type[torch.optim.Optimizer]:
-        ...
+        if optimizer_name == 'Adam':
+            return OptimizerClassFactory.create_adam_optimizer_class()
 
     @staticmethod
     def create_optimizer(
@@ -23,4 +24,8 @@ class OptimizerClassFactory:
         optimizer_name: str,
     ) -> torch.optim.Optimizer:
         ...
+
+    @staticmethod
+    def create_adam_optimizer_class():
+        return torch.optim.Adam
 
