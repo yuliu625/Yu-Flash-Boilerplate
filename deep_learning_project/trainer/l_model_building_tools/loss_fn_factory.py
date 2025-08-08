18669bbe082c9e0ff39db96f974a159f4695698d
yu <yul1024@outlook.com>
2025-08-08 22:36:29 +0800

    Update loss_fn_factory.py

--- deep_learning_project/trainer/l_model_building_tools/loss_fn_factory.py
+++ deep_learning_project/trainer/l_model_building_tools/loss_fn_factory.py
@@ -18,18 +18,35 @@ class LossFnFactory:
     @staticmethod
     def create_loss_fn(
         loss_fn_name: Literal[
-            'cross_entropy', 'mse', 'binary_crossentropy'
+            'cross_entropy',
+            'mse',
         ],
         loss_fn_config: dict,
     ) -> torch.nn.Module:
         if loss_fn_name == 'cross_entropy':
             return LossFnFactory.create_cross_entropy_loss_fn(loss_fn_config=loss_fn_config)
 
+    """<!--分类任务-start-->"""
+
     @staticmethod
     def create_cross_entropy_loss_fn(
         loss_fn_config: dict,
     ) -> torch.nn.Module:
-        return torch.nn.CrossEntropyLoss(
-            **loss_fn_config,
-        )
+        return torch.nn.CrossEntropyLoss(**loss_fn_config)
+
+    """<!--分类任务-end-->"""
+
+    """<!--回归任务-start-->"""
+
+    @staticmethod
+    def create_mse_loss_fn(
+        loss_fn_config: dict,
+    ) -> torch.nn.Module:
+        return torch.nn.MSELoss(**loss_fn_config)
+
+    """<!--回归任务-end-->"""
+
+    """<!--自定义-start-->"""
+
+    """<!--自定义-end-->"""
 
