dd68870f9185f2017d712818584c5e50174181c8
yu <yul1024@outlook.com>
2025-08-09 16:56:37 +0800

    Update test_models.py

--- tests/unit_tests/test_models/test_models.py
+++ tests/unit_tests/test_models/test_models.py
@@ -1,22 +1,30 @@
 """
 测试整体模型的输入输出形状是否复合预期。
+
+注意:
+    - 对于module的测试不需要使用dataloader，仅需输入预期形状的tensor。
 """
 
 from __future__ import annotations
+import pytest
+
+from deep_learning_project.torch_models import TorchModelFactory
+
+import torch
 
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
 
 
-# """在这里模拟输入，不需要真的使用dataloader。"""
-# torch.Size([1, 19, 3, 224, 224])
-# example_input_array = torch.rand(1, 5, 3, 224, 224)
-# model = NormalModel(
-#     {
-#
-#     }
-# )
-# outputs = model(example_input_array)
-# print(outputs)
-# print(outputs.shape)
+class TestTorchModels:
+    def test_torch_models(self, torch_model_name, torch_model_config):
+        torch_model = TorchModelFactory.create_torch_model(
+            torch_model_name=torch_model_name,
+            torch_model_config=torch_model_config,
+        )
+        # torch.Size([1, 19, 3, 224, 224])
+        example_input_array = torch.rand(1, 5, 3, 224, 224)
+        outputs = torch_model(example_input_array)
+        print(outputs)
+        print(outputs.shape)
 
