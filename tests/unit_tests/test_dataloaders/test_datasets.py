61e75c60fc3aa1790afa09ea7a41fc75972f6f85
yu <yul1024@outlook.com>
2025-08-09 16:56:39 +0800

    Update test_datasets.py

--- tests/unit_tests/test_dataloaders/test_datasets.py
+++ tests/unit_tests/test_dataloaders/test_datasets.py
@@ -3,12 +3,28 @@
 """
 
 from __future__ import annotations
+import pytest
+
+from deep_learning_project.torch_dataloaders.torch_datasets.torch_dataset_factory import TorchDatasetFactory
 
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
 
 
-# len(dataset)
-# dataset[0]
-# dataset[1]
+class TestDatasets:
+    def test_train_dataset(self, train_dataset_config):
+        train_dataset = TorchDatasetFactory.create_train_dataset(
+            dataset_config=train_dataset_config,
+        )
+        print(len(train_dataset))
+        print(train_dataset[0])
+        print(train_dataset[1])
+
+    def test_val_dataset(self, val_dataset_config):
+        val_dataset = TorchDatasetFactory.create_train_dataset(
+            dataset_config=val_dataset_config,
+        )
+        print(len(val_dataset))
+        print(val_dataset[0])
+        print(val_dataset[1])
 
