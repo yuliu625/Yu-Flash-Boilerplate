4332b1ead3aea4fc4cb6ac6a78765043c11c01dc
yu <yul1024@outlook.com>
2025-08-09 16:56:40 +0800

    Update test_dataloaders.py

--- tests/unit_tests/test_dataloaders/test_dataloaders.py
+++ tests/unit_tests/test_dataloaders/test_dataloaders.py
@@ -1,31 +1,46 @@
 """
 测试dataloader是否能正常加载数据。
 
-测试dataloader和测试collate_fn是一体的。
+注意:
+    - 测试dataloader和测试collate_fn是一体的。
 
-结合测试dataset的情况，可以确定collate_fn是否按照预期工作。
+目的:
+    - 查看batch的结果。
+    - 结合测试dataset的情况，可以确定collate_fn是否按照预期工作。
 """
 
 from __future__ import annotations
+import pytest
+
+from deep_learning_project.torch_dataloaders.torch_datasets.torch_dataset_factory import TorchDatasetFactory
+from deep_learning_project.torch_dataloaders.torch_dataloader_factory import TorchDataLoaderFactory
 
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
 
 
-# """在这里测试dataloader及其collate_fn的情况。"""
-# from torch_datasets import DFDataset
-# from torch.utils.data import DataLoader
-#
-# # 这里可以直接使用dataset包中已经测试通过的dataset和其config。
-# common_dataset = DFDataset({
-#     'control_file': {'path': r''},
-#     'file': {'file_dir_path': r''},
-#     'processor': {'path': r''},
-# })
-#
-# # 只要输出一次，batch_size大于1，并且有填充。
-# dataloader = DataLoader(common_dataset, batch_size=2, collate_fn=collate_fn)
-# for batch in dataloader:
-#     print(batch)
-#     break
+class TestDataLoaders:
+    def test_train_dataloader(self, train_dataset_config, train_dataloader_config):
+        train_dataset = TorchDatasetFactory.create_train_dataset(
+            dataset_config=train_dataset_config,
+        )
+        train_dataloader = TorchDataLoaderFactory.create_train_dataloader(
+            train_dataset=train_dataset,
+            train_dataloader_config=train_dataloader_config,
+        )
+        for batch in train_dataloader:
+            print(batch)
+            break
+
+    def test_val_dataloader(self, val_dataset_config, val_dataloader_config):
+        val_dataset = TorchDatasetFactory.create_val_dataset(
+            dataset_config=val_dataset_config,
+        )
+        val_dataloader = TorchDataLoaderFactory.create_val_dataloader(
+            val_dataset=val_dataset,
+            val_dataloader_config=val_dataloader_config,
+        )
+        for batch in val_dataloader:
+            print(batch)
+            break
 
