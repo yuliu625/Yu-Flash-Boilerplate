b4c9ddeeb89bd902e933801430468d9a40c9becf
yu <yul1024@outlook.com>
2025-08-09 16:56:42 +0800

    Create test_data_processors.py

---
+++ tests/unit_tests/test_dataloaders/test_data_processors.py
@@ -0,0 +1,17 @@
+"""
+测试data_processor对数据处理的结果。
+"""
+
+from __future__ import annotations
+import pytest
+
+from deep_learning_project.torch_dataloaders.torch_datasets.data_processors import DataProcessor
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class TestDataProcessor:
+    def test_data_processor(self, data):
+        ...
+
