329ef56c083f782efb35c357c8172d36a0a26273
yu <yul1024@outlook.com>
2025-08-09 16:57:16 +0800

    Update __init__.py

--- deep_learning_project/torch_dataloaders/__init__.py
+++ deep_learning_project/torch_dataloaders/__init__.py
@@ -1,6 +1,11 @@
 """
-基于torch定义的数据加载器。
+基于原生torch的dataloader的所相关的实现。
+
+使用场景:
+    - 原生torch: 直接在兼容torch的框架种使用dataloader。
+    - lightning: 实际上并不直接使用dataloader，而是定义lightning的data-module。
 """
 
-from .torch_dataloader_factory import DataLoaderFactory
+from .torch_datasets import TorchDatasetFactory
+from .torch_dataloader_factory import TorchDataLoaderFactory
 
